49321dc85bbb15eac4b4b15813a1d7d4b54b0aee
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 13:37:21 +0200

    commit

--- js/zdarzenia-eventy.js
+++ js/zdarzenia-eventy.js
@@ -23,19 +23,60 @@ thirdLink.removeEventListener('click', alarm);*/
 
 /* efekt najechania napisu po najechaniu */
 
-var mainHeader = document.getElementById('main-header'); /* - przypisujemy naglowek str do zmiennej */
+/*var mainHeader = document.getElementById('main-header');  - przypisujemy naglowek str do zmiennej */
 
-function resize(e) { /*przkeazujemy zdarzenie jako parametr funkcji */
-	/*console.log(e.type);*/
+/*function resize(e) { przkeazujemy zdarzenie jako parametr funkcji 
+	console.log(e.type);
+	
 	if (e.type == 'mouseover') {
 		mainHeader.style.fontSize = "70px";
 	} else {
 		mainHeader.style.fontSize = "inherit";
 	}
 	
+}*/
+
+/*mainHeader.onmouseover = resize; // - wywolanie funkcji resize po najechaniu myszka na naglowek 
+
+mainHeader.onmouseout = resize;  //- wywolanie funkcji resize po zjecaniu myszka z naglowka*/
+ 
+
+function klikHeader() {
+	console.log("kliknales w header");
+	
 }
 
-mainHeader.onmouseover = resize; /* - wywolanie funkcji resize po najechaniu myszka na naglowek */
+document.getElementsByTagName("header")[0].onclick = klikHeader; //wywolaj funkcje na elemencie header
 
-mainHeader.onmouseout = resize;  /*- wywolanie funkcji resize po zjecaniu myszka z naglowka */
- 
+
+function klikH1(e) {
+	e.stopPropagation(); //zatrzymanie propaginacji - funkcja zatrzymuje sie na tym elemencie i dalej nie idzie do rodzicow 
+	console.log("kliknales w h1");
+	
+}
+
+document.getElementsByTagName("h1")[0].onclick = klikH1;
+//wywolaj funkcje na elemencie h1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
