86981f7a12890a50a8313770c8e47645818e24aa
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 10:37:13 +0200

    commit

--- js/wezly-nodes.js
+++ js/wezly-nodes.js
@@ -5,7 +5,15 @@ var pierwszyDiv = document.getElementById('parFirst');
 console.log(pierwszyDiv.parentNode);
 console.log(pierwszyDiv.parentElement);*/
 
-/*console.log(pierwszyDiv.childNodes);*/
+console.log(pierwszyDiv.childNodes);
 /*console.log(pierwszyDiv.childNodes[3]);
 console.log(pierwszyDiv.children);*/
 
+/*
+console.log(pierwszyDiv.firstChild);
+console.log(pierwszyDiv.lastChild);
+*/
+
+var pierwszyChild = pierwszyDiv.childNodes[2];
+console.log(pierwszyChild);
+console.log(pierwszyChild.nextSibling.nodeType);
