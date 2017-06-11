950e9d9cc0797fa4e1c15478910934648081b5cc
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 10:16:51 +0200

    commit

---
+++ js/wezly-nodes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var pierwszyDiv = document.getElementById('parFirst');
+/*console.log(pierwszyDiv);
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);*/
+
+console.log(pierwszyDiv.childNodes);
