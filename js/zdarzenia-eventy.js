2531b76393af983279c09f49096912e617af0f0e
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 12:18:53 +0200

    commit

--- js/zdarzenia-eventy.js
+++ js/zdarzenia-eventy.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var secondLink = document.getElementsByTagName('a')[1];
+/* tworzymy zmienna z drugim linkiem */
+
+console.log(secondLink);
+function alarm(e) {
+	
+}
+
+secondLink.onclick = alarm;
