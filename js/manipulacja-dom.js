21db2634e5d91e2dc91a9aa89c7a160834623523
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 11:28:25 +0200

    commit

---
+++ js/manipulacja-dom.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var mainHeader = document.getElementById('main-header');
+/* przypisujemy naglowek strony do zmiennej */
+/*console.log(mainHeader);*/
+
+mainHeader.innerHTML = "Treść nagłówka";
+/* dodajemy tresc do naglowka */
+
+var link = document.getElementsByClassName('link')[0];
+/* przypisujemy pierwszy link do zmiennej */
+
+console.log(link);
+
+link.href = "http://akademia108.pl"; 
+/* nadpisujemy atrybut href w linki */
+
--- js/tworzenie-dodawanie-usuwanie-wezlow.js
+++ js/tworzenie-dodawanie-usuwanie-wezlow.js
@@ -1,1 +1,21 @@
 "use strict";
+
+var istniejacyWezel = document.getElementById('parFirst').children[3];
+/* przypisz do zmiennej istniejacy wezel */
+
+console.log(document.getElementById('parFirst').children);
+console.log(istniejacyWezel);
+
+var newElement = document.createElement('p'); 
+/* tworzymy nowy element */
+
+var newElementContent = document.createTextNode('To jest nowy paragraf');
+/* nowy tekst dla p */
+
+newElement.appendChild(newElementContent);
+
+istniejacyWezel.appendChild(newElement);
+
+console.log(newElement);
+
+istniejacyWezel.removeChild(newElement);
